00d4d0c1a98801e2ef13058010b1320f052dacab
boraedis <bora.edis@gmail.com>
2022-05-18 11:01:05 +0400

    entry homepage work

--- src/entry.js
+++
@@ -1,7 +0,0 @@
-import { db, app, auth, processQuery } from "./firebaseInit.js";
-
-
-var query = processQuery()
-
-const today = new Date();
-document.getElementById('date').innerHTML = today.toDateString()
